b8cdb8c561166dddb8e0ce5133d08bdcb35abc5c
name <email@example.ru>
2022-07-12 00:15:08 +0300

    any comment

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,20 @@
+import React from "react";
+import MainPage from "../../pages/Main-page/Main-page";
+
+type AboutFilm = {
+  title: string;
+  genre: string;
+  year: number
+};
+
 function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+  return (
+    <MainPage
+      title={'The Grand Budapest Hotel'}
+      genre={'драма'}
+      year={2014}
+    />
+  );
 }
 
 export default App;
