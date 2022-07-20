a7d510739eacc82f363717c9f4aa76bab022188f
name <email@example.ru>
2022-07-20 16:47:21 +0300

    new

---
+++ project/src/mocks/films.ts
@@ -0,0 +1,54 @@
+import { title } from "process";
+import { FilmsCollection } from "../types/films";
+
+export const films: FilmsCollection[] = [
+   { 
+    title: 'Big Buck Bunny',
+    genre: '2008',
+    year: 'Animation',
+    video: 'https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm',
+    id: 1,
+}, { 
+    title: 'A blast',
+    genre: '2014',
+    year: 'Thriller',
+    video: 'https://upload.wikimedia.org/wikipedia/commons/5/53/A_BLAST_Trailer_with_engl._subtitles.webm',
+    id: 2,
+}, { 
+    title: 'Last Vegas',
+    genre: '2013',
+    year: 'Comedy',
+    video: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Last_Vegas_-_Official_Trailer_-HD-.webm',
+    id: 3,
+}, { 
+    title: 'Het Bombardement',
+    genre: 'Drama',
+    year: '2013',
+    video: 'https://upload.wikimedia.org/wikipedia/commons/9/96/Het_Bombardement.webm',
+    id: 4,
+}, { 
+    title: 'Oorlogsgeheimen',
+    genre: '2014',
+    year: 'Drama',
+    video: 'https://upload.wikimedia.org/wikipedia/commons/b/be/Oorlogsgeheimen_3-juli_oorlog.webm',
+    id: 5,
+}, { 
+    title: 'Daglicht',
+    genre: 'Drama',
+    year: '2013',
+    video: 'https://upload.wikimedia.org/wikipedia/commons/9/93/Daglicht_11_april.webm',
+    id: 6,
+}, { 
+    title: 'Het Diner',
+    genre: 'Drama',
+    year: '2013',
+    video: 'https://upload.wikimedia.org/wikipedia/commons/7/7e/Het_Diner.webm',
+    id: 7,
+}, { 
+    title: 'Smoorverliefd',
+    genre: 'Comedy',
+    year: '2013',
+    video: 'https://upload.wikimedia.org/wikipedia/commons/5/5a/Smoorverliefd_12_september.webm',
+    id: 8,
+},
+];
---
+++ project/src/types/films.ts
@@ -0,0 +1,7 @@
+export type FilmsCollection = {
+  title: string;
+  genre: string;
+  year: string;
+  video: string;
+  id: number;
+};
